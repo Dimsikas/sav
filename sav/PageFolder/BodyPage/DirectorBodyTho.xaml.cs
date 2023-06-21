e76bb2f4975343df3f20d083d7aa657b959be748
Миронов Владислав <vlad.banana.syca@yandex.ru>
2023-06-21 09:50:11 +0300

    a;ksfas

---
+++ sav/PageFolder/BodyPage/DirectorBodyTho.xaml.cs
@@ -0,0 +1,16 @@
+﻿using sav.NewFolder1;
+using System.Linq;
+using System.Windows.Controls;
+
+namespace sav.PageFolder.BodyPage
+{
+    public partial class DirectorBodyTho : Page
+    {
+        public DirectorBodyTho()
+        {
+            InitializeComponent();
+            AppConectClass.DB = new SavEntities();
+            MainList.ItemsSource = AppConectClass.DB.Orders.ToList();
+        }
+    }
+}
